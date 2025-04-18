4216f7c4455170f7d0f5e67b5c82cc43ee9d7ea3
[rmahavenkat] <rmahavenkat@gmail.com>
2025-04-18 12:53:07 -0500

    rebase 1

--- src/test/java/dsAlgo_stepDefinition/Array_SD.java
+++ src/test/java/dsAlgo_stepDefinition/Array_SD.java
@@ -7,6 +7,7 @@ import ds_Algo_PageFactory.Array_PF;
 import ds_Algo_PageFactory.BasePageFactory;
 import io.cucumber.java.en.*;
 
+
 public class Array_SD extends BasePageFactory {
 
 	Array_PF array = new Array_PF();
--- src/test/java/dsAlgo_stepDefinition/hooks.java
+++ src/test/java/dsAlgo_stepDefinition/hooks.java
@@ -32,15 +32,14 @@ public class hooks {
 		LoggerReader.info("browser initialized");
 		LoggerReader.info("browser opened get driver");
 		driverfactory.getDriver().get(ConfigReader.getApplicationUrl());
-
 	}
-
 	@AfterStep
 	public void afterstep(Scenario scenario) throws IOException {
 
 		if (scenario.isFailed()) {
 
 			LoggerReader.error("Steps Failed, Taking Screenshot");
+
 			// For allure reports
 			TakesScreenshot screenshotTaker = (TakesScreenshot) driverfactory.getDriver();
 			final byte[] screenshot = screenshotTaker.getScreenshotAs(OutputType.BYTES);
